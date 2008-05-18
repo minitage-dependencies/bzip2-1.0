b8e31cb8282bb114ecb52aaa47617987474d8f68
kiorky <devnull@localhost>
2008-05-18 22:16:33 +0200

    0.4 uniformization

--- hooks/cmmi-hooks.py
+++ hooks/cmmi-hooks.py
@@ -1,27 +1,25 @@
 import os,shutil
 def copyto(dest, cplist):
     if not os.path.isdir(dest):
-        os.mkdir(dest)
+        os.makedirs(dest)
     for file in cplist:
         shutil.copy(file, dest)
 
-def bz2(options,buildout):
+def bz2(options, buildout):
     compile_dir=options["compile-directory"]
-    if os.path.isdir(compile_dir):
-        contents = os.listdir(compile_dir)
-        if len(contents) == 1:
-            os.chdir(compile_dir)
-            os.chdir(contents[0])
+    cwd = os.getcwd()
+    os.chdir(compile_dir)
 
-    lib_dest=options['location']+"/lib"
+    lib_dest=os.path.join(options['location'], 'lib')
     lib_cplist=['libbz2.a', 'libbz2.def', 'libbz2.dsp', ]
-    copyto(lib_dest,lib_cplist)
+    copyto(lib_dest, lib_cplist)
 
-    bin_dest=options['location']+"/bin"
+    bin_dest=os.path.join(options['location'], 'bin')
     bin_cplist=['bzdiff', 'bzip2recover', 'bzip2', 'bzgrep']
-    copyto(bin_dest,bin_cplist)
+    copyto(bin_dest, bin_cplist)
 
-    include_dest=options['location']+"/include"
+    include_dest=os.path.join(options['location'], 'unclude')
     include_cplist=['bzlib.h', 'bzlib_private.h']
-    copyto(include_dest,include_cplist)
+    copyto(include_dest, include_cplist)
+    os.chdir(cwd)
 
